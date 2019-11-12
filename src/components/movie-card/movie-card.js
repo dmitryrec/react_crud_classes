746fb24438a01d9c87d9c42679847e643d192a22
unknown <recfrag@gmail.com>
2019-11-12 20:52:03 +0300

    fix styles

--- src/components/movie-card/movie-card.js
+++ src/components/movie-card/movie-card.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './movies-card.css';
 
 export default class MovieCard extends Component {
     render() {
         const {movie} = this.props
         return (
-            <div>
+            <div className="moviesCardItem">
                 <h3>{movie.Title}</h3>
                 <img src={movie.Poster}/>
                 <p>year: {movie.Year}</p>
