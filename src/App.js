4385d324fd13e860de00be1b53f12a9a733864e9
unknown <recfrag@gmail.com>
2019-11-12 15:47:10 +0300

    nabvar basic

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const films = [
-  {
-    title: 'Matrix',
-    rate: 10
-  },
-  {
-    title: 'Equilibrium',
-    rate: 8
-  },
-];
-
-const FilmContainer = ({ title, rate }) => {
-  return (
-    <>
-      <h3>{title}</h3>
-      <p>Rating: {rate}</p>
-    </>
-  )
-}
+import Navbar from './components/navbar'
 
 export default class App extends Component {
 
   render() {
+
     return (
       <div className="App">
-        {films.map(film => <FilmContainer {...film} key={Math.random()} />)}
+        <Navbar/>
       </div>
     );
   }
